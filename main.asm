35e5b50c4240ed94c09e5020f31cacbbf024b3f4
Vivien B <vivienbihl@gmail.com>
2019-02-04 23:26:28 +0100

    Use OAM buffer, then copy to OAM via DMA

--- main.asm
+++ main.asm
@@ -2,79 +2,113 @@
 .include "include/reg.inc"
 .import init_reg
 
-SPRITE_X  = $0000
-UPDATE_X  = $0001
-DIRECTION = $0002
-TORSO     = $0003
-LEGS      = $0004
-DUMMY     = $0005
+OAML_BUF_START = $0100
+OAMH_BUF_START = $0300
 
-;----- Assembler Directives ----------------------------------------------------
-.p816                           ; tell the assembler this is 65816 code
-;-------------------------------------------------------------------------------
+.p816
 
-;----- Includes ----------------------------------------------------------------
 .segment "DATA"
-    SpriteData:
-        .incbin "assets/background.png.vra" ; $a80 bytes
-        .incbin "assets/mario.png.vra" ; $800 bytes
-    ColorData:
-        .incbin "assets/background.png.pal" ; $20 bytes
-        .incbin "assets/mario.png.pal" ; $20 bytes
+
+SpriteData:
+    .incbin "assets/background.png.vra" ; $a80 bytes
+    .incbin "assets/mario.png.vra" ; $800 bytes
+ColorData:
+    .incbin "assets/background.png.pal" ; $20 bytes
+    .incbin "assets/mario.png.pal" ; $20 bytes
 
 .segment "STARTUP"
+
 NMIStub:
     jml NMIHandler
 ResetStub:
     jml ResetHandler
-;-------------------------------------------------------------------------------
 
 .segment "CODE"
-;-------------------------------------------------------------------------------
-;   This is the entry point of the demo
-;-------------------------------------------------------------------------------
+
 .proc   ResetHandler
         sei                     ; disable interrupts
         clc                     ; clear the carry flag
         xce                     ; switch the 65816 to native (16-bit mode)
         jsr init_reg
 
-        ; reset custom memory locations
-        stz SPRITE_X
-        stz UPDATE_X
-        stz DIRECTION
-        stz TORSO
-        lda #$20
-        sta LEGS
-
         rep #$10
         sep #$20
 .a8
 .i16
+
+        ; Load dummy data in WRAM, set all sprites at position $101 (out of screen)
+        ; first load $01 in all sprites X coord (LSB)
+        lda #$01
+set_x_lsb:
+        sta OAML_BUF_START, x
+        inx
+        inx
+        inx
+        inx
+        cpx #$0200
+        bne set_x_lsb
+
+        ; then, set all sprite X MSB to 1
+        lda #$55
+set_x_msb:
+        sta OAML_BUF_START, x
+        inx
+        sta OAML_BUF_START, x
+        inx
+        cpx #$0220
+        bne set_x_msb
+
+        ; once that's done, load our sprite data
+        ldx #$0000
+        ; TORSO
+        lda #(256/2 - 16)
+        sta OAML_BUF_START, x
+        inx
+        lda #(224/2 - 16)
+        sta OAML_BUF_START, x
+        inx
+        stz OAML_BUF_START, x
+        inx
+        stz OAML_BUF_START, x
+        inx
+        ; LEGS
+        lda #(256/2 - 16)
+        sta OAML_BUF_START, x
+        inx
+        lda #(224/2)
+        sta OAML_BUF_START, x
+        inx
+        lda #$20
+        sta OAML_BUF_START, x
+        inx
+        stz OAML_BUF_START, x
+
+        ; reset those two sprites X MSB
+        ldx #$0000
+        lda #%01010000
+        sta OAMH_BUF_START, x
+
         ; -------------
-        ; OAM CLEAR VIA DMA
+        ; LOAD our dummy data into OAM via DMA
         ; -------------
         stz OAMADDL
         stz OAMADDH
 
         lda #$04 ; OAMDATA $21`04`
         sta BBAD0
-        ; load $ff to WRAM:$0005
-        lda #$ff
-        sta DUMMY
-        ; from WRAM address $00 (A bus address)
-        lda #$00
+        ; from WRAM address $7e:0100 (A bus address)
+        lda #$7e
         sta A1T0B
-        ldx #$0005 ; DUMMY address
+        ldx #$0100
         stx A1T0L
         ; total number of bytes to transfer (OAM is $220 bytes)
         ldx #$0220
         stx DAS0L
-        ; DMA params : A to B, don't increment (keep reading wram:$00
-        lda #$0a
+        ; DMA params : A to B
+        lda #$00
         sta DMAP0
         ; initiate DMA via channel 0 (LSB = channel 0, MSB channel 7)
-        lda #%00000001
+        lda #$01
         sta MDMAEN
 
         ; -------------
@@ -122,23 +156,11 @@ ResetStub:
         sep #$30
 .a8
 .i8
-        lda #$61 ; 16/32, start @ $2000
+        ; Set sprite size to 16/32, start @ VRAM $2000
+        lda #$61
         sta OBJSEL
 
-        ; set OAM address to $100
-        stz OAMADDL
-        lda #$01
-        sta OAMADDH
-        ; reset sx bits for first 4 objs
-        lda #$00
-        sta OAMDATA
-        ; set OAM address to $000
-        stz OAMADDL
-        stz OAMADDH
-
-        ; make Objects visible
-        jsr draw_sprite
-
+        ; enables sprites
         lda #$10
         sta TM
         ; release forced blanking, set screen to full brightness
@@ -150,135 +172,15 @@ ResetStub:
 
         jmp GameLoop            ; all initialization is done
 .endproc
-;-------------------------------------------------------------------------------
 
-;-------------------------------------------------------------------------------
-;   After the ResetHandler will jump to here
-;-------------------------------------------------------------------------------
-; .smart ; keep track of registers widths
 .proc   GameLoop
         wai                     ; wait for NMI / V-Blank
 
-        lda UPDATE_X
-        inc
-        sta UPDATE_X
-        cmp #$10
-        bne continue_gameloop ; skip moving
-        stz UPDATE_X
-
-        lda SPRITE_X
-        ldx DIRECTION
-
-        cpx #$01
-        beq go_left
-
-go_right:
-        adc #$10
-        cmp #$f0
-        bne update_position ; when A == 240, switch direction
-        lda #$f0
-        inx
-        bra update_position
-go_left:
-        sbc #$10
-        cmp #$00
-        bne update_position ; when A == 0, switch direction
-        dex
-        lda #$00
-
-update_position:
-        sta SPRITE_X
-        stx DIRECTION
-
-        lda TORSO
-        ina
-        ina
-        sta TORSO
-        cmp #$07
-        bcc update_legs
-        lda #$02
-        sta TORSO
-
-update_legs:
-        lda LEGS
-        ina
-        ina
-        sta LEGS
-        cmp #$27
-        bcc continue_gameloop
-        lda #$22
-        sta LEGS
-
-
-
-
-continue_gameloop:
-
         jmp GameLoop
 .endproc
-;-------------------------------------------------------------------------------
 
-;-------------------------------------------------------------------------------
-;   Will be called during V-Blank
-;-------------------------------------------------------------------------------
 .proc   NMIHandler
         lda RDNMI               ; read NMI status, acknowledge NMI
 
-        jsr draw_sprite
-
         rti
-.endproc
-;-------------------------------------------------------------------------------
-
-;-----
-; X = sprite x
-; Y = sprite y
-; A = sprite #
-;-----
-.proc set_sprite_data
-        pha
-        phx
-        phy
-
-        stx OAMDATA    ; horizontal position
-        sty OAMDATA    ; vertical position
-        sta OAMDATA    ; name of sprite
-
-        lda DIRECTION
-        cmp #$01
-        bne noflip
-        lda #$40       ; no flip, prio 0, palette 0
-        bra return
-noflip:
-        lda #$00
-return:
-        sta OAMDATA
-
-        ply
-        plx
-        pla
-        rts
-.endproc
-
-.proc draw_sprite
-; TODO optimize this mess
-        pha
-        phx
-        phy
-
-        clc
-
-        ldx SPRITE_X
-        ldy #$20 ; Y coord
-        lda TORSO ; char #
-        jsr set_sprite_data
-
-        ldy #$30 ; Y coord
-        lda LEGS ; char #
-        jsr set_sprite_data
-
-        ply
-        plx
-        pla
-        rts
 .endproc
