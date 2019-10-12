edcc1c44ac9f4a78603aa24ca507ee09328dbcd8
Vivien B <vivienbihl@gmail.com>
2019-10-12 13:31:16 +0200

    Move regexes to module

--- mini_assembler/mini_assembler.rb
+++ mini_assembler/mini_assembler.rb
@@ -2,6 +2,8 @@ require 'yaml'
 require 'readline'
 require 'byebug'
 
+require_relative './regexes'
+
 def write(nbytes)
   data = (1..nbytes).map { |b| '00' }
   File.open('test.bin', 'w+b') do |file|
@@ -10,46 +12,13 @@ def write(nbytes)
 end
 
 class MiniAssembler
-
-  HEX8  = '\$?([0-9a-f]{1,2})'
-  HEX16 = '\$?([0-9a-f]{3,4})'
-  HEX24 = '\$?([0-9a-f]{5,6})'
-
-  MODES_REGEXES = {
-    acc:   /^$/,
-    imp:   /^$/,
-    imm:   /^#{HEX8}$/i,
-    iml:   /^#{HEX16}$/i,
-    imm8:  /^##{HEX8}$/i,
-    imm16: /^##{HEX16}$/i,
-    sr:    /^#{HEX8},S$/i,
-    dp:    /^#{HEX8}$/i,
-    dpx:   /^#{HEX8},X$/i,
-    dpy:   /^#{HEX8},Y$/i,
-    idp:   /^\(#{HEX8}\)$/i,
-    idx:   /^\(#{HEX8},X\)$/i,
-    idy:   /^\(#{HEX8}\),Y$/i,
-    idl:   /^\[#{HEX8}\]$/i,
-    idly:  /^\[#{HEX8}\],Y$/i,
-    isy:   /^\(#{HEX8},S\),Y$/i,
-    abs:   /^#{HEX16}$/i,
-    abx:   /^#{HEX16},X$/i,
-    aby:   /^#{HEX16},Y$/i,
-    abl:   /^#{HEX24}$/i,
-    alx:   /^#{HEX24},X$/i,
-    ind:   /^\(#{HEX16}\)$/i,
-    iax:   /^\(#{HEX16},X\)$/i,
-    ial:   /^\[#{HEX16}\]$/i,
-    rel:   /^#{HEX16}$/i,
-    rell:  /^#{HEX16}$/i,
-    bm:    /^#{HEX8},#{HEX8}$/i
-  }
+  include Regexes
 
   def initialize
     @normal_mode = true
-    @lorom = true
-    @bank_size = @lorom ? 0x8000 : 0x10000
-    @memory = (0..255).map { |bank| (1..@bank_size).map { |b| rand(0..255).to_s(16).rjust(2, '0') } }
+    @bank_no = 0x100
+    @bank_size = 0x10000
+    @memory = (0..@bank_no-1).map { |bank| (0..@bank_size-1).map { |b| rand(0..255).to_s(16).rjust(2, '0') } }
     @current_addr = 0
     @current_bank_no = 0
     @accumulator = 1
@@ -75,9 +44,9 @@ class MiniAssembler
       if line == '!'
         @normal_mode = false
         return
-      elsif /^[0-9a-fA-F]+$/ =~ line
+      elsif MiniAssembler::BYTE_LOC =~ line
         return current_bank[line.to_i(16)]
-      elsif matches = /^([0-9a-fA-F]+)\.+([0-9a-fA-F]+)$/.match(line)
+      elsif matches = MiniAssembler::BYTE_RANGE.match(line)
         start_addr = matches[1].to_i(16)
         end_addr = matches[2].to_i(16)
         end_addr = start_addr if end_addr < start_addr
@@ -95,17 +64,17 @@ class MiniAssembler
           end
           ["#{@current_bank_no.to_s(16).rjust(2, '0')}/#{line_addr.to_s(16).rjust(4, '0')}-", *row].join(' ')
         end.join("\n")
-      elsif matches = /^([0-9a-fA-F]+):([0-9a-fA-F ]+)$/.match(line)
+      elsif matches = MiniAssembler::BYTE_SEQUENCE.match(line)
         addr = matches[1].to_i(16)
         bytes = matches[2].delete(' ').scan(/.{1,2}/).map { |b| b.to_i(16).to_s(16).rjust(2, '0') }
         current_bank[addr..addr+bytes.length-1] = bytes
         return
-      elsif matches = /^([0-9a-fA-F]{2})\/$/.match(line)
+      elsif matches = MiniAssembler::SWITCH_BANK.match(line)
         # check if memory has enough bank.
         # lowrom bank size = 0x8000
         # hirom bank size =  0x10000
         @current_bank_no = matches[1].to_i(16)
-      elsif matches = /^([01])=([xm])$/i.match(line)
+      elsif matches = MiniAssembler::FLIP_MX_REG.match(line)
         val = matches[1]
         reg = matches[2]
 
--- mini_assembler/regexes.rb
+++ mini_assembler/regexes.rb
@@ -1,8 +1,42 @@
 module Regexes
-  HEX8  = '\$?([0-9a-f]{1,2})'
-  HEX16 = '\$?([0-9a-f]{3,4})'
-  HEX24 = '\$?([0-9a-f]{5,6})'
+  HEX_DIGIT = '[0-9a-f]'
+  HEX8  = "\$?(#{HEX_DIGIT}{1,2})"
+  HEX16 = "\$?(#{HEX_DIGIT}{3,4})"
+  HEX24 = "\$?(#{HEX_DIGIT}{5,6})"
 
-  IMM8  = "\##{HEX8}"
-  IMM16 = "\##{HEX16}"
+  MODES_REGEXES = {
+    acc:   /^$/,
+    imp:   /^$/,
+    imm:   /^#{HEX8}$/i,
+    iml:   /^#{HEX16}$/i,
+    imm8:  /^##{HEX8}$/i,
+    imm16: /^##{HEX16}$/i,
+    sr:    /^#{HEX8},S$/i,
+    dp:    /^#{HEX8}$/i,
+    dpx:   /^#{HEX8},X$/i,
+    dpy:   /^#{HEX8},Y$/i,
+    idp:   /^\(#{HEX8}\)$/i,
+    idx:   /^\(#{HEX8},X\)$/i,
+    idy:   /^\(#{HEX8}\),Y$/i,
+    idl:   /^\[#{HEX8}\]$/i,
+    idly:  /^\[#{HEX8}\],Y$/i,
+    isy:   /^\(#{HEX8},S\),Y$/i,
+    abs:   /^#{HEX16}$/i,
+    abx:   /^#{HEX16},X$/i,
+    aby:   /^#{HEX16},Y$/i,
+    abl:   /^#{HEX24}$/i,
+    alx:   /^#{HEX24},X$/i,
+    ind:   /^\(#{HEX16}\)$/i,
+    iax:   /^\(#{HEX16},X\)$/i,
+    ial:   /^\[#{HEX16}\]$/i,
+    rel:   /^#{HEX16}$/i,
+    rell:  /^#{HEX16}$/i,
+    bm:    /^#{HEX8},#{HEX8}$/i
+  }
+
+  BYTE_LOC = /^#{HEX_DIGIT}{1,4}$/i
+  BYTE_RANGE = /^(#{HEX_DIGIT}{1,4})\.+(#{HEX_DIGIT}{1,4})$/i
+  BYTE_SEQUENCE = /^(#{HEX_DIGIT}{1,4}):([0-9a-f ]+)$/i
+  SWITCH_BANK = /^([0-9a-f]{2})\/$/i
+  FLIP_MX_REG = /^([01])=([xm])$/i
 end
