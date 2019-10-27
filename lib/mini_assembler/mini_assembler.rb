999407e1d11f6845ba7b7683a8af6b5410c2c581
vivi168 <vivi168@users.noreply.github.com>
2019-10-27 15:15:30 +0100

    Make a gem and cleanup (#2)
    * Move demo file to a folder
    
    * Attempt to make gem
    
    * Add missing files to gemspec
    
    * Update readme

---
+++ lib/mini_assembler/mini_assembler.rb
@@ -0,0 +1,326 @@
+module SnesUtils
+  Readline.completion_proc = proc do |input|
+    Definitions::OPCODES_DATA.map { |row| row[:mnemonic] }
+                               .select { |mnemonic| mnemonic.upcase.start_with?(input.upcase) }
+  end
+
+  class MiniAssembler
+    def initialize(filename = nil)
+      if filename && File.file?(filename)
+        file = File.open(filename)
+        @memory = file.each_byte.map { |b| hex(b) }
+      else
+        @memory = []
+      end
+
+      @normal_mode = true
+
+      @current_addr = 0
+      @current_bank_no = 0
+      @accumulator_flag = 1
+      @index_flag = 1
+
+      @next_addr_to_list = 0
+    end
+
+    def run
+      while line = getline
+        result = parse_line(line.strip.chomp)
+        puts result if result
+      end
+
+      puts
+    end
+
+    def hex(num, rjust_len = 2)
+      num.to_s(16).rjust(rjust_len, '0').upcase
+    end
+
+    def write(filename = 'out.smc')
+      filename = filename.empty? ? 'out.smc' : filename
+
+      File.open(filename, 'w+b') do |file|
+        file.write([@memory.map { |i| i ? i : '00' }.join].pack('H*'))
+      end
+
+      filename
+    end
+
+    def incbin(filename, addr)
+      return 0 unless File.file?(filename)
+
+      file = File.open(filename)
+      bytes = file.each_byte.map { |b| hex(b) }
+
+      replace_memory_range(addr, addr + bytes.size - 1, bytes)
+
+      bytes.size
+    end
+
+    def read(filename)
+      return 0 unless File.file?(filename)
+
+      file = File.open(filename)
+
+      instructions = []
+
+      file.each_with_index do |raw_line, line_no|
+        line = raw_line.split(';').first.strip.chomp
+        next if line.empty?
+
+        instruction, length, address = parse_instruction(line)
+        return "Error at line #{line_no + 1}" unless instruction
+
+        instructions << [instruction, length, address]
+        @current_addr = address + length
+      end
+
+      disassembled_instructions = []
+      instructions.map do |instruction_arr|
+        instruction, length, address = instruction_arr
+        replace_memory_range(address, address+length-1, instruction)
+        disassembled_instructions << disassemble_range(address, 1, length > 2).join
+      end
+
+      return disassembled_instructions
+    end
+
+    def detect_opcode_data_from_mnemonic(mnemonic, operand)
+      Definitions::OPCODES_DATA.detect do |row|
+        mode = row[:mode]
+        regex = Definitions::MODES_REGEXES[mode]
+        row[:mnemonic] == mnemonic && regex =~ operand
+      end
+    end
+
+    def detect_opcode_data_from_opcode(opcode, force_length)
+      Definitions::OPCODES_DATA.detect do |row|
+        if row[:m]
+          accumulator_flag = force_length ? 0 : @accumulator_flag
+          row[:opcode] == opcode && row[:m] == accumulator_flag
+        elsif row[:x]
+          index_flag = force_length ? 0 : @index_flag
+          row[:opcode] == opcode && row[:x] == index_flag
+        else
+          row[:opcode] == opcode
+        end
+      end
+    end
+
+    def full_address(address)
+      (@current_bank_no << 16) | address
+    end
+
+    def address_human(addr=nil)
+      address = full_address(addr || @current_addr)
+      bank = address >> 16
+      addr = (((address>>8)&0xFF) << 8) | (address&0xFF)
+      "#{hex(bank)}/#{hex(addr, 4)}"
+    end
+
+    def memory_loc(address)
+      @memory[full_address(address)]
+    end
+
+    def memory_range(start_addr, end_addr)
+      start_full_addr = full_address(start_addr)
+      end_full_addr = full_address(end_addr)
+
+      return [] if start_full_addr > end_full_addr || start_full_addr >= @memory.length
+
+      @memory[start_full_addr..end_full_addr]
+    end
+
+    def replace_memory_range(start_addr, end_addr, bytes)
+      start_full_addr = full_address(start_addr)
+      end_full_addr = full_address(end_addr)
+
+      @memory[start_full_addr..end_full_addr] = bytes
+
+      true
+    end
+
+    def getline
+      prompt = @normal_mode ? "(#{@accumulator_flag}=m #{@index_flag}=x)*" : "(#{address_human})!"
+      Readline.readline(prompt, true)
+    end
+
+    def parse_line(line)
+      if @normal_mode
+        if line == '!'
+          @normal_mode = false
+          return
+        elsif matches = Definitions::WRITE_REGEX.match(line)
+          filename = matches[1].strip.chomp
+          out_filename = write(filename)
+          return "Written #{@memory.size} bytes to file #{out_filename}"
+        elsif matches = Definitions::READ_REGEX.match(line)
+          filename = matches[1].strip.chomp
+          return read(filename)
+        elsif matches = Definitions::INCBIN_REGEX.match(line)
+          start_addr = matches[1].to_i(16)
+          filename = matches[2].strip.chomp
+          nb_bytes = incbin(filename, start_addr)
+
+          return "Inserted #{nb_bytes} bytes at #{address_human(start_addr)}"
+        elsif Definitions::BYTE_LOC_REGEX =~ line
+          return memory_loc(line.to_i(16))
+        elsif matches = Definitions::BYTE_RANGE_REGEX.match(line)
+          start_addr = matches[1].to_i(16)
+          end_addr = matches[2].to_i(16)
+
+          padding_count = start_addr % 8
+          padding = (1..padding_count).map { |b| '  ' }
+          arr = memory_range(start_addr, end_addr)
+          return if arr.empty?
+
+          padded_arr = arr.insert(8-padding_count, *padding).each_slice(8).to_a
+          return padded_arr.each_with_index.map do |row, idx|
+            if idx == 0
+              line_addr = start_addr
+            else
+              line_addr = start_addr - padding_count + idx * 8
+            end
+            ["#{address_human(line_addr)}-", *row].join(' ')
+          end.join("\n")
+        elsif matches = Definitions::BYTE_SEQUENCE_REGEX.match(line)
+          addr = matches[1].to_i(16)
+          bytes = matches[2].delete(' ').scan(/.{1,2}/).map { |b| hex(b.to_i(16)) }
+          replace_memory_range(addr, addr + bytes.length - 1, bytes)
+          return
+        elsif matches = Definitions::DISASSEMBLE_REGEX.match(line)
+          start = matches[1].empty? ? @next_addr_to_list : matches[1].to_i(16)
+          return disassemble_range(start, 20).join("\n")
+        elsif matches = Definitions::SWITCH_BANK_REGEX.match(line)
+          target_bank_no = matches[1].to_i(16)
+          @current_bank_no = target_bank_no
+          @current_addr = @current_bank_no << 16
+          @next_addr_to_list = 0
+          return
+        elsif matches = Definitions::FLIP_MX_REG_REGEX.match(line)
+          val = matches[1]
+          reg = matches[2]
+
+          if reg.downcase == 'm'
+            @accumulator_flag = val.to_i
+          elsif reg.downcase == 'x'
+            @index_flag = val.to_i
+          end
+
+          return
+        end
+      else
+        if line == ''
+          @normal_mode = true
+          return
+        else
+          instruction, length, address = parse_instruction(line)
+          return 'error' unless instruction
+
+          replace_memory_range(address, address+length-1, instruction)
+          @current_addr = address + length
+          return disassemble_range(address, 1, length > 2).join
+        end
+      end
+    end
+
+    def parse_address(line)
+      return @current_addr if line.index(':').nil?
+      line.split(':').first.to_i(16)
+    end
+
+    def parse_instruction(line)
+      current_address = parse_address(line)
+      instruction = line.split(':').last.split(' ')
+      mnemonic = instruction[0].upcase
+      raw_operand = instruction[1].to_s
+
+      opcode_data = detect_opcode_data_from_mnemonic(mnemonic, raw_operand)
+
+      return unless opcode_data
+
+      opcode = hex(opcode_data[:opcode])
+      mode = opcode_data[:mode]
+      length = opcode_data[:length]
+
+      operand_matches = Definitions::MODES_REGEXES[mode].match(raw_operand)
+      if mode == :bm
+        operand = "#{operand_matches[1]}#{operand_matches[2]}".to_i(16)
+      else
+        operand = operand_matches[1]&.to_i(16)
+      end
+
+      if operand
+        if %i[rel rell].include? mode
+          relative_addr = operand - current_address - length
+          return if mode == :rel && (relative_addr < -128 || relative_addr > 127)
+          return if mode == :rell && (relative_addr < -32768 || relative_addr > 32767)
+
+          relative_addr = (2**(8*(length-1))) + relative_addr if relative_addr < 0
+
+          param_bytes = hex(relative_addr, 2*(length-1)).scan(/.{2}/).reverse.join
+        else
+          param_bytes = hex(operand, 2*(length-1)).scan(/.{2}/).reverse.join
+        end
+      end
+
+      encoded_result = "#{opcode}#{param_bytes}"
+
+      return [encoded_result.scan(/.{2}/), length, current_address]
+    end
+
+    def auto_update_flags(opcode, operand)
+      if 0xc2 == opcode
+        @index_flag = 0 if (operand & 0x10) == 0x10
+        @accumulator_flag = 0 if (operand & 0x20) == 0x20
+      elsif 0xe2 == opcode
+        @index_flag = 1 if (operand & 0x10) == 0x10
+        @accumulator_flag = 1 if (operand & 0x20) == 0x20
+      end
+    end
+
+    def disassemble_range(start, count, force_length = false)
+      next_idx = start
+      instructions = []
+      count.times do
+        byte = memory_loc(next_idx)
+        break unless byte
+        opcode = byte.to_i(16)
+
+        opcode_data = detect_opcode_data_from_opcode(opcode, force_length)
+
+        mnemonic = opcode_data[:mnemonic]
+        mode = opcode_data[:mode]
+        length = opcode_data[:length]
+
+        format = Definitions::MODES_FORMATS[mode]
+
+        operand = memory_range(next_idx+1, next_idx+length-1).reverse.join.to_i(16)
+
+        hex_encoded_instruction = memory_range(next_idx, next_idx+length-1)
+        prefix = ["#{address_human(next_idx)}:", *hex_encoded_instruction].join(' ')
+
+        auto_update_flags(opcode, operand)
+
+        if mode == :bm
+          operand = operand.to_s(16).scan(/.{2}/).map { |op| op.to_i(16) }
+        elsif %i[rel rell].include? mode
+          limit = mode == :rel ? 0x7f : 0x7fff
+          offset = mode == :rel ? 0x100 : 0x10000
+          rjust_len = mode == :rel ? 2 : 4
+          relative_addr = operand > limit ? operand - offset : operand
+          relative_addr_s = "#{relative_addr.positive? ? '+' : '-'}#{hex(relative_addr.abs, rjust_len)}"
+          absolute_addr = next_idx + length + relative_addr
+          absolute_addr += 0x10000 if absolute_addr.negative?
+          operand = [absolute_addr, relative_addr_s]
+        end
+
+        instructions << "#{prefix.ljust(30)} #{format % [mnemonic, *operand]}"
+        next_idx += length
+      end
+
+      @next_addr_to_list = next_idx
+      return instructions
+    end
+  end
+end
---
+++ lib/png2snes/png2snes.rb
@@ -0,0 +1,114 @@
+module SnesUtils
+  class Png2Snes
+    def initialize(file_path, bpp:4, alpha:nil)
+      @file_path = file_path
+      @file_dir = File.dirname(@file_path)
+      @file_name = File.basename(@file_path, File.extname(@file_path))
+      @image = ChunkyPNG::Image.from_file(@file_path)
+      @pixels = pixels_to_bgr5
+
+      @palette = @pixels.uniq
+      @char_size = 8
+
+      raise ArgumentError, 'BPP must be 2, 4, or 8' unless [2, 4, 8].include? bpp
+      @bpp = bpp
+
+      raise ArgumentError, 'Image width and height must be a multiple of sprite size' if (@image.width % @char_size != 0) or (@image.height % @char_size != 0)
+
+      alpha_first if alpha
+      fill_palette
+    end
+
+    def pixels_to_bgr5
+      @image.pixels.map do |c|
+        r = ((c >> 24) & 0xff) >> 3
+        g = ((c >> 16) & 0xff) >> 3
+        b = ((c >>  8) & 0xff) >> 3
+
+        r | (g << 5) | (b << 10)
+      end
+    end
+
+    def alpha_first
+    end
+
+    def fill_palette
+      target_size = 2**@bpp
+      missing_colors = target_size - @palette.count
+      raise ArgumentError, "Palette size too large for target BPP (#{@palette.count})" if missing_colors < 0
+
+      @palette += [0] * missing_colors
+    end
+
+    def write hex, file_path
+      File.open(file_path, 'w+b') do |file|
+        file.write([hex.join].pack('H*'))
+      end
+    end
+
+    def write_palette
+      palette_hex = @palette.map { |c| ('%04x' % c).scan(/.{2}/).reverse.join }
+      write palette_hex, File.expand_path("#{@file_name}-pal.bin", @file_dir)
+    end
+
+    def pixel_indices
+      pix_idx = @pixels.map { |p| @palette.index(p) }
+      pix_idx_bin = pix_idx.map { |i| "%0#{@bpp}b" % i }
+      pix_idx_bin.map { |i| i.reverse }
+    end
+
+    def extract_sprites
+      pixel_idx = pixel_indices
+
+      sprite_per_row = @image.width / @char_size
+      sprite_per_col = @image.height / @char_size
+      sprite_per_sheet =  sprite_per_row * sprite_per_col
+
+      sprites = []
+      (0..sprite_per_sheet-1).each do |s|
+        sprite = []
+        (0..@char_size-1).each do |r|
+          offset = (s/sprite_per_row)*sprite_per_row * @char_size**2 + s % sprite_per_row * @char_size
+          sprite += pixel_idx[offset + r*sprite_per_row*@char_size, @char_size]
+        end
+        sprites.push(sprite)
+      end
+
+      sprites
+    end
+
+    def extract_bitplanes sprite
+      bitplanes = []
+      (0..@bpp-1).each do |plane|
+        bitplanes.push sprite.map { |p| p[plane] }
+      end
+
+      bitplanes
+    end
+
+    def write_image
+      sprite_per_row = @image.width / @char_size
+      sprites = extract_sprites
+      sprites_bitplanes = sprites.map { |s| extract_bitplanes s }
+
+      image_bits = ""
+      sprites_bitplanes.each do |sprite_bitplanes|
+        sprite_bitplane_pairs = sprite_bitplanes.each_slice(2).to_a
+
+        bitplane_bits = ""
+        sprite_bitplane_pairs.each do |bitplane|
+          (0..@char_size-1).each do |r|
+            offset = r*@char_size
+            bitplane_bits += bitplane[0][offset, @char_size].join + bitplane[1][offset, @char_size].join
+          end
+        end
+        image_bits += bitplane_bits
+
+      end
+
+      image_hex = image_bits.scan(/.{8}/).map { |b| "%02x" % b.to_i(2) }
+      write image_hex, File.expand_path("#{@file_name}.bin", @file_dir)
+    end
+
+  end
+end
